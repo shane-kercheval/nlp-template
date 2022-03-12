47d7e35b02b09e52f0ad98d0d1471b2c7dc3fe46
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2022-03-11 16:24:12 -0800

    added tokenize/remove_stop_words

--- source/executables/helpers/text_processing.py
+++ source/executables/helpers/text_processing.py
@@ -1,5 +1,5 @@
 from typing import List, Callable
-
+import nltk
 import regex as re
 
 
@@ -17,6 +17,32 @@ def tokenize(text: str) -> list:
         text: string of text
     """
     return re.findall(r'[\w-]*\p{L}[\w-]*', text)
+
+
+def get_stop_words(source: str = 'nltk') -> set:
+    if source == 'nltk':
+        stop_words = set(nltk.corpus.stopwords.words('english'))
+    else:
+        raise ValueError("Only 'nlkt' source supported for stopwords.")
+
+    return stop_words
+
+
+def remove_stop_words(tokens: list, source: str = 'nltk') -> list:
+    """
+    Remove stop-words from `tokens` in a list of tokens.
+
+    From:
+        Blueprints for Text Analytics Using Python
+        by Jens Albrecht, Sidharth Ramachandran, and Christian Winkler
+        (O'Reilly, 2021), 978-1-492-07408-3.
+         https://github.com/blueprints-for-text-analytics-python/blueprints-text/blob/master/ch01/First_Insights.ipynb
+
+    Args:
+        tokens: string of text
+        source: source of the stop-words to use
+    """
+    return [t for t in tokens if t.lower() not in get_stop_words(source=source)]
 
 
 def prepare(text: str, pipeline: List[Callable]) -> list:
--- source/tests/test_text_processing.py
+++ source/tests/test_text_processing.py
@@ -1,6 +1,6 @@
 import unittest
 
-from source.executables.helpers.text_processing import tokenize
+from source.executables.helpers.text_processing import tokenize, remove_stop_words
 from source.tests.helpers import get_test_file_path
 
 
@@ -9,10 +9,17 @@ class TestTextProcessing(unittest.TestCase):
     def test__open_dict_like_file(self):
         self.assertTrue(True)
 
-    def test__timer(self):
+    def test__tokenize(self):
         sentence = "This is a sentence; it has punctuation, etc.. It also has numbers. It's a dumb sentence."
         tokens = tokenize(sentence)
 
         with open(get_test_file_path('text_processing/tokenize__simple.txt'), 'w') as file:
+            file.write('|'.join(tokens))
+
+    def test__remove_stop_words(self):
+        sentence = "This is a sentence; it has punctuation, etc.. It also has numbers. It's a dumb sentence."
+        tokens = tokenize(sentence)
+        tokens = remove_stop_words(tokens)
+        with open(get_test_file_path('text_processing/remove_stop_words__simple.txt'), 'w') as file:
             file.write('|'.join(tokens))
 
