4cd940dbe97d736b0ee3ab08083654e642b729bd
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2022-03-12 13:41:48 -0800

    added count_tokens

--- source/tests/helpers.py
+++ source/tests/helpers.py
@@ -1,5 +1,8 @@
 import os
-from helpsk.utility import is_debugging
+
+import pandas as pd
+from helpsk.pandas import print_dataframe
+from helpsk.utility import is_debugging, redirect_stdout_to_file
 
 
 def get_test_file_path(file_path) -> str:
@@ -12,3 +15,13 @@ def get_test_file_path(file_path) -> str:
 
     path = os.path.join(path, 'test_files', file_path)
     return path
+
+
+def dataframe_to_text_file(dataframe, file_name):
+    if os.path.isfile(file_name):
+        os.remove(file_name)
+    pd.set_option('display.max_rows', None)
+
+    with redirect_stdout_to_file(file_name):
+        print_dataframe(dataframe)
+    assert os.path.isfile(file_name)
--- source/tests/test_text_processing.py
+++ source/tests/test_text_processing.py
@@ -1,7 +1,9 @@
 import unittest
 
-from source.executables.helpers.text_processing import tokenize, remove_stop_words, prepare
-from source.tests.helpers import get_test_file_path
+import pandas as pd
+
+from source.executables.helpers.text_processing import tokenize, remove_stop_words, prepare, count_tokens
+from source.tests.helpers import get_test_file_path, dataframe_to_text_file
 
 
 class TestTextProcessing(unittest.TestCase):
@@ -34,3 +36,27 @@ class TestTextProcessing(unittest.TestCase):
         tokens = prepare(sentence)
         with open(get_test_file_path('text_processing/prepare__simple.txt'), 'w') as file:
             file.write('|'.join(tokens))
+
+    def test__count_tokens(self):
+        sentence = "This is a sentence; it has punctuation, etc.. It also has numbers. Nevermind, it doesn't have numbers. It's a dumb sentence."  # noqa
+        tokens = prepare(sentence)
+
+        dataframe_to_text_file(count_tokens(tokens, min_frequency=1),
+                               get_test_file_path('text_processing/count_tokens__list__min_freq_1.txt'))
+        dataframe_to_text_file(count_tokens(tokens, min_frequency=2),
+                               get_test_file_path('text_processing/count_tokens__list__min_freq_2.txt'))
+
+        dataframe_to_text_file(count_tokens([tokens, tokens], min_frequency=1),
+                               get_test_file_path('text_processing/count_tokens__list_of_lists__min_freq_1.txt'))
+        dataframe_to_text_file(count_tokens([tokens, tokens], min_frequency=2),
+                               get_test_file_path('text_processing/count_tokens__list_of_lists__min_freq_2.txt'))
+
+        dataframe_to_text_file(count_tokens(pd.Series(tokens), min_frequency=1),
+                               get_test_file_path('text_processing/count_tokens__series_strings__min_freq_1.txt'))
+        dataframe_to_text_file(count_tokens(pd.Series(tokens), min_frequency=2),
+                               get_test_file_path('text_processing/count_tokens__series_strings__min_freq_2.txt'))
+
+        dataframe_to_text_file(count_tokens(pd.Series([tokens, tokens]), min_frequency=1),
+                               get_test_file_path('text_processing/count_tokens__series_list__min_freq_1.txt'))
+        dataframe_to_text_file(count_tokens(pd.Series([tokens, tokens]), min_frequency=2),
+                               get_test_file_path('text_processing/count_tokens__series_list__min_freq_2.txt'))
