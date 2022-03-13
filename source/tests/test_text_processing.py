f9c9a0d916e920b9fb7c2871232304e783f091cd
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2022-03-13 13:54:03 -0700

    unit tests

--- source/tests/test_text_processing.py
+++ source/tests/test_text_processing.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 import pandas as pd
@@ -8,6 +9,12 @@ from source.tests.helpers import get_test_file_path, dataframe_to_text_file
 
 class TestTextProcessing(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        un_debates = pd.read_csv(get_test_file_path('text_processing/un-general-debates-blueprint__sample.csv'))
+        un_debates['tokens'] = un_debates['text'].map(prepare)
+        cls.un_debates = un_debates
+
     def test__open_dict_like_file(self):
         self.assertTrue(True)
 
@@ -17,6 +24,11 @@ class TestTextProcessing(unittest.TestCase):
 
         with open(get_test_file_path('text_processing/tokenize__simple.txt'), 'w') as file:
             file.write('|'.join(tokens))
+
+        token_list = self.un_debates.text.map(tokenize)
+        token_list = ['|'.join(x) for x in token_list]
+        with open(get_test_file_path('text_processing/tokenize__un_debates.txt'), 'w') as file:
+            file.write('\n'.join(token_list))
 
     def test__remove_stop_words(self):
         sentence = "This is a sentence; it has punctuation, etc.. It also has numbers. It's a dumb sentence."
@@ -36,6 +48,10 @@ class TestTextProcessing(unittest.TestCase):
         tokens = prepare(sentence)
         with open(get_test_file_path('text_processing/prepare__simple.txt'), 'w') as file:
             file.write('|'.join(tokens))
+
+        token_list = ['|'.join(x) for x in self.un_debates['tokens']]
+        with open(get_test_file_path('text_processing/prepare__un_debates.txt'), 'w') as file:
+            file.write('\n'.join(token_list))
 
     def test__count_tokens(self):
         sentence = "This is a sentence; it has punctuation, etc.. It also has numbers. Nevermind, it doesn't have numbers. It's a dumb sentence."  # noqa
@@ -60,3 +76,7 @@ class TestTextProcessing(unittest.TestCase):
                                get_test_file_path('text_processing/count_tokens__series_list__min_freq_1.txt'))
         dataframe_to_text_file(count_tokens(pd.Series([tokens, tokens]), min_frequency=2),
                                get_test_file_path('text_processing/count_tokens__series_list__min_freq_2.txt'))
+
+    def test__tf_idf(self):
+        self.un_debates
+
