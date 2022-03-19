6219631c51cd7e40a428d5fef730b32f1049e4e9
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2022-03-19 16:31:06 -0700

    Update text_preparation.py

--- source/library/text_preparation.py
+++ source/library/text_preparation.py
@@ -4,6 +4,18 @@ import source.library.regex_patterns as rx
 
 
 def clean(text: str):
+    """
+    Applies various functions and regex patterns to clean the string.
+
+    Args:
+        text:
+            text to clean
+
+    Blueprints for Text Analytics Using Python
+        by Jens Albrecht, Sidharth Ramachandran, and Christian Winkler
+        (O'Reilly, 2021), 978-1-492-07408-3.
+        https://github.com/blueprints-for-text-analytics-python/blueprints-text/blob/master/ch04/Data_Preparation.ipynb
+    """
     # convert html escapes like &amp; to characters.
     text = html.unescape(text)
     # tags like <tab>
