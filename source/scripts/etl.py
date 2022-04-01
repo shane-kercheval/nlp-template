8496e3c840e52fd2d022b950337179cdb667cee0
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2022-04-01 14:50:52 -0700

    Update etl.py

--- source/scripts/etl.py
+++ source/scripts/etl.py
@@ -2,13 +2,16 @@ import click
 import pandas as pd
 import sys
 import os
+from math import ceil
+
+import regex
 
 sys.path.append(os.getcwd())
 from source.library.utilities import get_logger, Timer  # noqa
 from source.library.text_analysis import impurity  # noqa
 from source.library.text_cleaning_simple import prepare, get_n_grams, get_stop_words, tokenize  # noqa
 from source.library.text_preparation import clean, predict_language  # noqa
-from source.library.spacy import create_spacy_pipeline, custom_tokenizer, extract_from_doc, doc_to_dataframe  # noqa
+from source.library.spacy import create_spacy_pipeline, custom_tokenizer, extract_from_doc, doc_to_dataframe, extract_lemmas  # noqa
 
 
 @click.group()
@@ -39,6 +42,9 @@ def extract():
 def transform():
     logger = get_logger()
     logger.info("Transforming Data")
+    ####
+    # UN Debate Data
+    ####
     with Timer("Loading UN Generate Debate Dataset"):
         un_debates = pd.read_pickle('artifacts/data/raw/un-general-debates-blueprint.pkl')
 
@@ -60,6 +66,61 @@ def transform():
     with Timer("Saving processed UN Debate dataset to /artifacts/data/processed/un-general-debates-blueprint.pkl"):
         un_debates.to_pickle('artifacts/data/processed/un-general-debates-blueprint.pkl')
 
+    with Timer("Creating UN dataset that is Per Year/Country/Paragraph"):
+        paragraphs_series = un_debates["text"].map(lambda text: regex.split(r'\.\s*\n', text))
+        # flatten the paragraphs keeping the years, countries
+        un_debates_paragraphs = pd.DataFrame(
+            [
+                {"year": year, "country": country, "text": paragraph}
+                for year, country, paragraphs in zip(un_debates["year"], un_debates["country_name"], paragraphs_series)
+                for paragraph in paragraphs if paragraph
+            ]
+        )
+        un_debates_paragraphs['text'] = un_debates_paragraphs['text'].str.strip()
+        un_debates_paragraphs = un_debates_paragraphs[un_debates_paragraphs['text'] != '']
+
+    # This takes about 30 minutes to run.
+    # with Timer("Cleaning UN Paragraph dataset "):
+    #     nlp = create_spacy_pipeline(stopwords_to_add={'dear', 'regards'},
+    #                                 stopwords_to_remove={'down'},
+    #                                 tokenizer=custom_tokenizer)
+    #
+    #     def text_lemmas(text: str) -> str:
+    #         text = clean(
+    #             text,
+    #             remove_angle_bracket_content=True,
+    #             remove_bracket_content=True,
+    #         )
+    #         lemmas = extract_from_doc(
+    #             doc=nlp(text),
+    #             all_lemmas=True,
+    #             partial_lemmas=False,
+    #             bi_grams=False,
+    #             adjectives_verbs=False,
+    #             nouns=False,
+    #             noun_phrases=False,
+    #             named_entities=False
+    #         )
+    #         return ' '.join(lemmas['all_lemmas'])
+    #
+    #     un_debates_paragraphs['clean_text'] = None
+    #     batch_size = 25000
+    #     from math import ceil
+    #     num_batches = ceil(len(un_debates_paragraphs) / batch_size)
+    #     for i in range(0, len(un_debates_paragraphs), batch_size):
+    #         logger.info(f"Processing Batch {round((i / batch_size) + 1)} of {num_batches}")
+    #         results = [text_lemmas(x) for x in un_debates_paragraphs['text'][i:i + batch_size]]
+    #         for j, result in enumerate(results):
+    #             un_debates_paragraphs['clean_text'].iloc[i + j] = result
+
+
+
+    with Timer("Saving UN Paragraphs dataset to /artifacts/data/processed/un-general-debates-paragraphs.pkl"):
+        un_debates_paragraphs.to_pickle('artifacts/data/processed/un-general-debates-paragraphs.pkl')
+
+    ####
+    # Processing Reddit Data
+    ####
     with Timer("Loading Reddit Dataset - Sampling 5K rows."):
         reddit = pd.read_pickle('artifacts/data/raw/reddit.pkl')
         reddit = reddit.sample(5000, random_state=42)
@@ -67,11 +128,10 @@ def transform():
     with Timer("Loading Reddit Dataset - Calculating Impurity"):
         reddit['impurity'] = reddit['post'].apply(impurity)
 
-    with Timer("Reddit - Cleaning Data"):
-        reddit['post_clean'] = reddit['post'].apply(clean,
-                                                    remove_bracket_content=False)
+    with Timer("Cleaning Data"):
+        reddit['post_clean'] = reddit['post'].apply(clean, remove_bracket_content=False)
     assert not reddit.isna().any().any()
-    with Timer("Reddit - Tokenizing & Extracting"):
+    with Timer("Tokenizing & Extracting"):
         nlp = create_spacy_pipeline(stopwords_to_add={'dear', 'regards'},
                                     stopwords_to_remove={'down'},
                                     tokenizer=custom_tokenizer)
@@ -80,8 +140,10 @@ def transform():
         for col in nlp_columns:
             reddit[col] = None
 
-        batch_size = 50
+        batch_size = 500
+        num_batches = ceil(len(reddit) / batch_size)
         for i in range(0, len(reddit), batch_size):
+            logger.info(f"Processing Batch {round((i / batch_size) + 1)} of {num_batches}")
             docs = nlp.pipe(reddit['post_clean'][i:i + batch_size])
             for j, doc in enumerate(docs):
                 for col, values in extract_from_doc(doc).items():
